dfbcfa53e32dabe9eff5da1ab0aa867d32784983
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-31 09:31:35 -0700

    first java assignment

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Butternut\nI am 9 years old\nMy hometown is Everett,WA");
+    }
+}
---
+++ greetings/Test.java
@@ -0,0 +1,13 @@
+// .java is where we write soure code, .class is the Byte code file when you run 'javac filename'
+// 'java filename' to run the code
+
+// everything is an object
+//file name should be capitalized!
+//semicolons are always required!
+
+public class Test {
+    //not all files will have a main method but there needs to be atleast one to run the app
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
