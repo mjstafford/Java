cadac998205c001edd2ebf73dbde486a050ea156
Michael <67665934+mjstafford@users.noreply.github.com>
2022-08-04 18:26:48 -0700

    core assignment complete

--- SpringProjects/Daikichi/src/main/java/com/mjstafford/daikichi/DaikichiController.java
+++ SpringProjects/Daikichi/src/main/java/com/mjstafford/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.mjstafford.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,19 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+	@RequestMapping("/travel/{city}")
+	public String city(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";			
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 }
 
