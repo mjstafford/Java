86897c86625b174544d83310bf6c32af7a873c74
Michael <67665934+mjstafford@users.noreply.github.com>
2022-08-09 13:09:44 -0700

    added edit functionality to books

--- SpringProjects/Books/src/main/java/com/mjstafford/books/controllers/BooksController.java
+++ SpringProjects/Books/src/main/java/com/mjstafford/books/controllers/BooksController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.mjstafford.books.models.Book;
 import com.mjstafford.books.services.BookService;
@@ -25,6 +27,7 @@ public class BooksController {
 	@Autowired						//does your dependency injection for you, otherwise need a constructor to instantiate it
 	BookService bookService;
 	
+	//get all books
 	@GetMapping("/books")
 	public String index(Model model) {
 		List<Book> books = bookService.allBooks();
@@ -33,7 +36,7 @@ public class BooksController {
 		return "index.jsp";
 	}
 	
-	
+	//get Book by id
 	@GetMapping("/books/{BookId}")
 	public String show(Model model, @PathVariable(value="BookId") Long BookId) {
 		
@@ -43,11 +46,13 @@ public class BooksController {
 		return "show.jsp";
 	}
 	
+	//get the books form page
 	@GetMapping("/books/new")
 	public String bookForm(@ModelAttribute("book") Book book) {
 		return "new.jsp";
 	}
 	
+	//submit form to create new book
 	@PostMapping("/books/new")
 	public String process(@Valid @ModelAttribute("book") Book book, BindingResult result) {
 //			@RequestParam("title") String title,
@@ -66,5 +71,27 @@ public class BooksController {
 		
 		return "redirect:/books";
 	}
+	
+	//edit book form show page
+    @GetMapping("/books/{id}/edit")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        return "edit.jsp";
+    }
+    
+    //edit book form process form submit
+    //Be sure to use 'id' in your route: value="/books/{id}". Using a different path variable name will create a new instance.
+    @RequestMapping(value="/books/{id}", method=RequestMethod.PUT)
+    public String update(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+        	//we can re-render this page if there are errors because it can never duplicate a submission in the db while an error exists
+        	//this also automatically keeps all of the users previous input into the fields
+            return "edit.jsp";
+        } else {
+            bookService.updateBook(book);
+            return "redirect:/books";
+        }
+    }
 	
 }
