def091539aa5ad225c4fa163fe56dd11d0fcc598
Michael <67665934+mjstafford@users.noreply.github.com>
2022-08-11 06:27:31 -0700

    dojos and ninjas starter code

---
+++ SpringProjects/Relationships/src/main/java/com/mjstafford/relationships/model/Person.java
@@ -0,0 +1,87 @@
+package com.mjstafford.relationships.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    
+    // one-to-one relationships
+    // "mappedBy="person" goes on the owner-side (aka a Person has a license. .. a license does not have a person)
+    // "cascade=CascadeType.ALL" goes on owner side, this means if a person is deleted so is the license since it only exists for the person
+    // "fetch=FetchType.LAZY" only grabs the associations when needed
+    @OneToOne(mappedBy="person", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    private License license;
+    
+    public Person() {   
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public License getLicense() {
+		return license;
+	}
+
+	public void setLicense(License license) {
+		this.license = license;
+	}
+    
+    
+}
